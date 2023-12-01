289702e1ee28c0d18da89487e86ef881ce0ae94b
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-11-30 23:14:46 -0300

    Create Adivinha_sequencia.cpp

---
+++ Adivinha_sequencia.cpp
@@ -0,0 +1,34 @@
+/*
+Vamos desenvolver um código que capacita o computador a prever a sequência de 3 números.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number_one{}, number_two{}, number_three{};
+
+	cout << "Escreva o primeiro numero da sequencia: ";
+	cin >> number_one;
+
+	cout << "Escreva o segundo numero da sequencia: ";
+	cin >> number_two;
+
+	cout << "Escreva o terceiro numero da sequencia: ";
+	cin >> number_three;
+
+	int i = 0;
+
+	while (number_one <= number_two) {
+		number_one += 1;
+		
+		i += 1;
+		if (number_one == number_two) {
+			cout << "A sequencia e de: " << i << " em " << i;
+		}
+		else {
+			continue;
+		}
+	}
+}
